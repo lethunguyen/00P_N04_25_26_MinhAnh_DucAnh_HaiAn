43c15aac228c917baa0dcc7fb83e14a7d2665119
Nguyen Le Duc Anh <23010246@st.phenikaa-uni.edu.vn>
2025-08-18 21:31:26 +0700

    first commit

--- MinhAnd_DucAnh_HaiAn_Project/src/App.java
+++ MinhAnd_DucAnh_HaiAn_Project/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Bia myBeer = new Bia();
+        myBeer.nhanbia = "bia cua toi.";
+        myBeer.ganNhanBia("Hanoi");
+
+        System.out.println("Bia : " + myBeer.layNhanBia());
     }
 }
--- MinhAnd_DucAnh_HaiAn_Project/src/App2.java
+++
@@ -1,5 +0,0 @@
-public class App2 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! from Hai An");
-    }
-}
