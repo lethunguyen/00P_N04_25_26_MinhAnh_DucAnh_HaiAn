14699e3828cccecb0237964e538d79e9696e0c0f
NmA3108 <nguyenanh31082k5@gmail.com>
2025-09-08 16:19:15 +0700

    Code demo Qly KhachHang

--- MinhAnd_DucAnh_HaiAn_Project/src/App.java
+++ MinhAnd_DucAnh_HaiAn_Project/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        Bia myBeer = new Bia();
+        KhachHang myBeer = new KhachHang();
         myBeer.nhanbia = "bia cua toi.";
         myBeer.ganNhanBia("Hanoi");
 
