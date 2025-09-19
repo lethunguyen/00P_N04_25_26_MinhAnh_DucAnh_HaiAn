7a9ce19e2a9e5766be36e60410cbc0727c4eacd7
chinmeomeo2865 <23011987@st.phenikaa-uni.edu.vn>
2025-09-19 14:40:16 +0700

    Bai cua Hai An

---
+++ MinhAnd_DucAnh_HaiAn_Project/README.md
@@ -0,0 +1,27 @@
+## Type of Object (Class)
+
+```
+
+public class Bia{
+
+    //Bien thanh vien
+
+    public String nhanbia;
+    public String lapbia;
+    public String chatlong;
+
+    //Phương thuc hoat dong: Methods: operation of object
+
+    public String layNhanBia(){
+          return nhanbia;
+    }
+
+    public String ganNhanBia(String nhan){
+        nhanbia = nhan;
+
+        return nhanbia;
+    }
+
+}
+
+```
---
+++ MinhAnd_DucAnh_HaiAn_Project/src/EmployeeTest.java
@@ -0,0 +1,27 @@
+public class EmployeeTest {
+    public static void main(String[] args) {
+        EmployeeManager manager = new EmployeeManager();
+
+        // CREATE
+        Employee e1 = new Employee("E001", "Nguyen Van A", 25, "0123456789", false);
+        Employee e2 = new Employee("E002", "Tran Thi B", 30, "9876543210", true);
+
+        manager.addEmployee(e1);
+        manager.addEmployee(e2);
+
+        // READ
+        manager.listEmployees();
+
+        // UPDATE
+        manager.updateEmployee("E001", "Nguyen Van A (Updated)", 26, "1111111111", true);
+
+        // READ agian
+        manager.listEmployees();
+
+        // DELETE
+        manager.removeEmployee("E002");
+
+        // READ last
+        manager.listEmployees();
+    }
+}
