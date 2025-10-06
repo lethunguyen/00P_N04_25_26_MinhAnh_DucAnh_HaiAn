aaf8f13cbf9c05fb18700037366b4882678a2603
NmA3108 <151888790+NmA3108@users.noreply.github.com>
2025-10-06 17:09:06 +0700

    Add files via upload

---
+++ MinhAnd_DucAnh_HaiAn_Project/EmployeeManager.java
@@ -0,0 +1,44 @@
+package Controller;
+
+import java.util.ArrayList;
+
+import Model.Employee;
+
+public class EmployeeManager {
+    private ArrayList<Employee> employees = new ArrayList<>();
+
+    // CREATE
+    public void addEmployee(Employee e) {
+        employees.add(e);
+        System.out.println("Added Employee: " + e.getName());
+    }
+
+    // READ
+    public void listEmployees() {
+        System.out.println("Employee List:");
+        for (Employee e : employees) {
+            System.out.println(e);
+        }
+    }
+
+    // UPDATE
+    public void updateEmployee(String id, String newName, int newAge, String newCccd, boolean newRole) {
+        for (Employee e : employees) {
+            if (e.getId().equals(id)) {
+                e.setName(newName);
+                e.setAge(newAge);
+                e.setCccd(newCccd);
+                e.setIsManager(newRole);
+                System.out.println("Updated Employee Information " + id);
+                return;
+            }
+        }
+        System.out.println("No Employee Found With ID: " + id);
+    }
+
+    // DELETE
+    public void removeEmployee(String id) {
+        employees.removeIf(e -> e.getId().equals(id));
+        System.out.println("Deleted Employee With ID: " + id);
+    }
+}
