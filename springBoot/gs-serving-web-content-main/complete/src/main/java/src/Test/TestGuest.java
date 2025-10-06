5b0ee3f6371715fc08455a8bd656f2112b458a65
chinmeomeo2865 <23011987@st.phenikaa-uni.edu.vn>
2025-10-06 17:08:07 +0700

    Add files via upload

---
+++ springBoot/gs-serving-web-content-main/complete/src/main/java/src/Test/TestGuest.java
@@ -0,0 +1,29 @@
+package Test;
+import Controller.GuestCRUD;
+import Model.Guest;
+public class TestGuest {
+    public static void main(String[] args) {
+        GuestCRUD dao = new GuestCRUD();
+
+        // Add guests
+        dao.add(new Guest("An", "G01", "0123456789", "Hanoi"));
+        dao.add(new Guest("Binh", "G02", "0987654321", "Danang"));
+
+        // Print all
+        System.out.println("Guest list:");
+        for (Guest g : dao.getAll()) {
+            System.out.println(g);
+        }
+
+        // Update
+        dao.update("G01", "An Updated", "0909999999", "HCM");
+
+        // Delete
+        dao.delete("G02");
+
+        System.out.println("After update & delete:");
+        for (Guest g : dao.getAll()) {
+            System.out.println(g);
+        }
+    }
+}
