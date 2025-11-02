0cf82690eb5b4dade645d66c3938028c9eace644
dav449 <23010246@st.phenikaa-uni.edu.vn>
2025-11-02 16:30:31 +0000

    LM 1.0

--- Library-Management/src/main/java/com/library/management/LibraryManagementApplication.java
+++ Library-Management/src/main/java/com/library/management/LibraryManagementApplication.java
@@ -2,10 +2,19 @@ package com.library.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class LibraryManagementApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(LibraryManagementApplication.class, args);
+    }
+
+    // ✅ Đặt bean ở đây — bên trong class
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
     }
 }
